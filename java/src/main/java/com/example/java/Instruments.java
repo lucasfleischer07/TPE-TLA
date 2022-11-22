5bb63f3063b85aa3a4d9bf4005c0f55b675029b6
Lucas Fleischer <lfleischer@itba.edu.ar>
2022-11-22 20:18:49 -0300

    Instrument list changed

--- java/src/main/java/com/example/java/Instruments.java
+++ java/src/main/java/com/example/java/Instruments.java
@@ -2,11 +2,7 @@ package com.example.java;
 
 public enum Instruments {
     PIANO((byte)0),
-    FLUTE((byte)75),
-    ELECTRIC_GUITAR((byte)27),
-    TRUMPET((byte)56),
-    BASS((byte)33);
-
+    FLUTE((byte)75);
 
     Instruments(byte value){
         this.value=value;
